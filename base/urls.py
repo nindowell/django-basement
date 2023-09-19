ba99dfa5eee7042f380424e0ec4eb8a1184e9551
nindo <nindowell@gmail.com>
2023-09-19 18:24:42 +0600

    gitignore update

--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index
-
-# Create your urls here.
-
-urlpatterns = [
-    path('', index, name='index'),
-]
--- base/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    context = {'var': 'hello world'}
-    return render(request, 'base/index.html', context)
